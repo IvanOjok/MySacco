c8667f5ab5c86cc53c06a9fa19d2b258db5ccbb9
OiJohnSteven <oisteven018@gmail.com>
2026-01-24 21:29:25 +0300

    Proj setup

---
+++ src/main/java/com/jambo/mysacco/MysaccoApplication.java
@@ -0,0 +1,13 @@
+package com.jambo.mysacco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysaccoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysaccoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jambo/mysacco/MysaccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jambo.mysacco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysaccoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
